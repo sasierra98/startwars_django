c2b4bf2d22d3d42283b9296bd75e0d8f53c6239b
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-10-21 23:27:12 -0500

    Planet model added

--- planets/models.py
+++ planets/models.py
@@ -1,4 +1,39 @@
 from django.db import models
+from django.utils.timezone import now
+
+from movies.models import Movie
 
 
-class Planet(models.Model): ...
+class Planet(models.Model):
+    name = models.CharField(
+        max_length=150
+    )
+
+    diameter = models.PositiveIntegerField()
+
+    climate = models.CharField(
+        max_length=20
+    )
+
+    gravity = models.FloatField()
+
+    terrain = models.CharField(
+        max_length=20
+    )
+
+    population = models.PositiveIntegerField()
+
+    movies = models.ManyToManyField(
+        to=Movie
+    )
+
+    created_at = models.DateTimeField(
+        default=now
+    )
+
+    updated_at = models.DateTimeField(
+        default=now
+    )
+
+    def __str__(self) -> str:
+        return f'{self.id}|{self.name}'
