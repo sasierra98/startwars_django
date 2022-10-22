8bab25c08df9e55b4ae43acd5223d8eb782fd690
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-10-21 23:25:53 -0500

    Movie model added

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.utils.timezone import now
 
 
-class Movie(models.Model): ...
+class Movie(models.Model):
+    name = models.CharField(
+        max_length=150
+    )
+
+    director = models.CharField(
+        max_length=150
+    )
+
+    producer = models.CharField(
+        max_length=150
+    )
+
+    release_date = models.DateField()
+
+    created_at = models.DateField(
+        default=now
+    )
+
+    updated_at = models.DateField(
+        default=now
+    )
+
+    def __str__(self) -> str:
+        return f'{self.id}|{self.name}'
